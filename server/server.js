e021f3cee20b13ee7f1e9d3191b622185cee398f
Giorgos Kavallieratos <egiokav@gmail.com>
2018-03-20 21:06:06 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat application',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createdAt: new Date().getTime()
+    });
+
 
     socket.on('createMessage', (message) => {
 
@@ -25,6 +37,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', () => {

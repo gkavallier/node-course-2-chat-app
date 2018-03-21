774a7dcbefc7d1c4bc86a5b29d0474900045cc28
Giorgos Kavallieratos <egiokav@gmail.com>
2018-03-21 19:19:31 +0200

    temporary commit3

--- server/utils/message.js
+++ server/utils/message.js
@@ -6,7 +6,7 @@ var generateMessage = (from, text) => {
     }
 };
 
-var generateLocationMessage = (from, latitude, longitude) = {
+var generateLocationMessage = (from, latitude, longitude) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
